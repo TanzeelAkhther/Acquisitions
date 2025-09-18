1c813aac619a079d9c382600d0f0a21d8b41c839
Tanzeel <tanzeelakther949@gmail.com>
2025-09-19 01:18:58 +0530

    Implement ESline and Prettier

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
-import express from "express";
+import express from 'express';
 
-const app = express();     // Create an Express application
+const app = express(); // Create an Express application
 
-const PORT = process.env.PORT || 3000;  // Define the port to run the server
+app.get('/', (req, res) => {
+  //
+  res.status(200).send('Hello from Acquisitions API!'); // Define a route handler for the root UR
+});
 
-app.get('/', (req, res) => {   //
-    res.status(200).send("Hello from Acquisitions API!")  // Define a route handler for the root UR
-})
-
-
-export default app;  // Export the Express application for use in other modules
+export default app; // Export the Express application for use in other modules
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import app from './app.js';
 
-const PORT = process.env.PORT || 3000;  // Define the port to run the server
+const PORT = process.env.PORT || 3000; // Define the port to run the server
 
-app.listen(PORT, () => { //start the server and listen on the defined port
-    console.log(`Listening on http://localhost:${PORT}...`);
-})
+app.listen(PORT, () => {
+  //start the server and listen on the defined port
+  console.log(`Listening on http://localhost:${PORT}...`);
+});
